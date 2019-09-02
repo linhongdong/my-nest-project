e3c846d96e750894286cc24e96cd1bdf9b94b177
林洪东 <1625125333@qq.com>
2019-09-02 17:25:10 +0800

    提交测试

---
+++ src/users/dto/createUser.dto.ts
@@ -0,0 +1,26 @@
+import { ApiModelProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+    @ApiModelProperty({
+        description: '用户 ID',
+        example: 0,
+    })
+    readonly userId: number;
+    @ApiModelProperty({
+        description: '用户名称',
+        example: '嘉文四世',
+        required: false,
+    })
+    readonly userName: number;
+    @ApiModelProperty({
+        description: '用户角色名称',
+        example: '德玛西亚',
+    })
+    readonly roleName: string;
+    @ApiModelProperty({
+        description: '用户角色名称',
+        example: 'DMXY',
+        enum: ['DMXY', 'AONY', 'AYD', 'FLEZD'],
+    })
+    readonly roleCode: string;
+}
